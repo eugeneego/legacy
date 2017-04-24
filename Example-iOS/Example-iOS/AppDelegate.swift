a411e42362f13a24bcee25baa027b0a6bdda1557
Eugene Egorov <eugene.ego@gmail.com>
2017-04-24 15:56:08 +0300

    Added example project.

---
+++ Example-iOS/Example-iOS/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  Example-iOS
+//
+//  Created by Eugene Egorov on 24/04/2017.
+//  Copyright © 2017 Eugene Egorov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+}
---
+++ Example-iOS/Example-iOS/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Example-iOS
+//
+//  Created by Eugene Egorov on 24/04/2017.
+//  Copyright © 2017 Eugene Egorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
