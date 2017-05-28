ff3e592073aacbd1f5cb114952d63c2f9f4cabd3
Eugene Egorov <eugene.ego@gmail.com>
2017-05-28 12:34:15 +0300

    As is transformer.

---
+++ Sources/Network/Transformers/Full/AsIsTransformer.swift
@@ -0,0 +1,22 @@
+//
+// AsIsTransformer
+// EE Utilities
+//
+// Copyright (c) 2017 Eugene Egorov.
+// License: MIT, https://github.com/eugeneego/utilities-ios/blob/master/LICENSE
+//
+
+public struct AsIsTransformer<From>: FullTransformer {
+    public typealias Source = From
+    public typealias Destination = From
+
+    public init() {}
+
+    public func transform(source value: Source) -> TransformerResult<Destination> {
+        return .success(value)
+    }
+
+    public func transform(destination value: Destination) -> TransformerResult<Source> {
+        return .success(value)
+    }
+}
