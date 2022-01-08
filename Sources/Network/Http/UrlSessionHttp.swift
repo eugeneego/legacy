84e623165af1d57e14bac736498b4107a8bdb2a8
Eugene Egorov <eugene.ego@gmail.com>
2022-01-09 00:59:28 +0300

    Updated http trust policy matching and logging

--- Sources/Network/Http/UrlSessionHttp.swift
+++ Sources/Network/Http/UrlSessionHttp.swift
@@ -19,22 +19,17 @@ open class UrlSessionHttp: Http {
 
     private let delegateObject: Delegate
 
-    private let logger: HttpLogger?
-
-    open var maxLoggingBodySize: Int {
-        get { logger?.maxBodySize ?? 0 }
-        set { logger?.maxBodySize = newValue }
-    }
+    private let logger: UrlSessionHttpLogger?
 
     public init(
         configuration: URLSessionConfiguration,
         responseQueue: DispatchQueue,
-        logger: Logger? = nil,
-        loggerTag: String = String(describing: UrlSessionHttp.self)
+        trustPolicies: [String: ServerTrustPolicy] = [:],
+        logger: UrlSessionHttpLogger? = nil
     ) {
         self.responseQueue = responseQueue
-        self.logger = logger.map { HttpLogger(logger: $0, loggerTag: loggerTag) }
-        delegateObject = Delegate(responseQueue: responseQueue)
+        self.logger = logger
+        delegateObject = Delegate(responseQueue: responseQueue, trustPolicies: trustPolicies)
         session = URLSession(configuration: configuration, delegate: delegateObject, delegateQueue: delegateObject.queue)
     }
 
@@ -77,11 +72,11 @@ open class UrlSessionHttp: Http {
         var startDate: Date = Date()
         let request: URLRequest
         let queue: DispatchQueue
-        let logger: HttpLogger?
+        let logger: UrlSessionHttpLogger?
 
         var internalCompletion: (URLResponse?, Error?) -> Void { { _, _ in } }
 
-        init(task: URLSessionTask, request: URLRequest, queue: DispatchQueue, logger: HttpLogger?) {
+        init(task: URLSessionTask, request: URLRequest, queue: DispatchQueue, logger: UrlSessionHttpLogger?) {
             self.task = task
             self.request = request
             self.queue = queue
@@ -162,7 +157,7 @@ open class UrlSessionHttp: Http {
             logger?.log(result) ?? ""
         }
 
-        init(task: URLSessionTask, destination: URL, request: URLRequest, queue: DispatchQueue, logger: HttpLogger?) {
+        init(task: URLSessionTask, destination: URL, request: URLRequest, queue: DispatchQueue, logger: UrlSessionHttpLogger?) {
             self.destination = destination
             super.init(task: task, request: request, queue: queue, logger: logger)
         }
@@ -171,14 +166,14 @@ open class UrlSessionHttp: Http {
     // MARK: - Delegate
 
     private class Delegate: NSObject, URLSessionDataDelegate, URLSessionDownloadDelegate {
-        var trustPolicies: [String: ServerTrustPolicy] = [:]
-        let queue: OperationQueue
+        var trustPolicies: [String: ServerTrustPolicy]
+        let queue: OperationQueue = OperationQueue()
         private let responseQueue: DispatchQueue
         private var tasks: [Task] = []
 
-        init(responseQueue: DispatchQueue) {
+        init(responseQueue: DispatchQueue, trustPolicies: [String: ServerTrustPolicy]) {
             self.responseQueue = responseQueue
-            queue = OperationQueue()
+            self.trustPolicies = trustPolicies
             queue.qualityOfService = .userInitiated
             queue.maxConcurrentOperationCount = 1
         }
@@ -202,7 +197,7 @@ open class UrlSessionHttp: Http {
             let space = challenge.protectionSpace
             let host = space.host
             let isServerTrust = space.authenticationMethod == NSURLAuthenticationMethodServerTrust
-            if isServerTrust, let serverTrust = space.serverTrust, let policy = trustPolicies[host] {
+            if isServerTrust, let serverTrust = space.serverTrust, let policy = trustPolicy(host: host) {
                 if policy.evaluate(serverTrust: serverTrust, host: host) {
                     disposition = .useCredential
                     credential = URLCredential(trust: serverTrust)
@@ -212,6 +207,20 @@ open class UrlSessionHttp: Http {
             }
 
             completionHandler(disposition, credential)
+        }
+
+        private func trustPolicy(host: String) -> ServerTrustPolicy? {
+            var components = host.components(separatedBy: ".")
+            repeat {
+                let host = components.isEmpty ? "*" : components.joined(separator: ".")
+                if let policy = trustPolicies[host] {
+                    return policy
+                }
+                if components.isEmpty {
+                    return nil
+                }
+                components.remove(at: 0)
+            } while true
         }
 
         // MARK: - Task
@@ -303,106 +312,19 @@ open class UrlSessionHttp: Http {
         }
     }
 
-    // MARK: - Log
-
-    private class HttpLogger {
-        private let logger: Logger
-        private let loggerTag: String
-        var maxBodySize: Int = 8192
-
-        init(logger: Logger, loggerTag: String) {
-            self.logger = logger
-            self.loggerTag = loggerTag
-        }
-
-        func log(_ request: URLRequest, date: Date) {
-            let tag = "←"
-            let body = request.httpBody.flatMap { data -> String? in
-                if let type = request.allHTTPHeaderFields?["Content-Type"], isText(type: type) && data.count <= maxBodySize {
-                    return String(data: data, encoding: .utf8) ?? String(data: data, encoding: .ascii)
-                } else {
-                    return "\(data.count) bytes"
-                }
-            } ?? request.httpBodyStream.map { _ in "stream" }
-            let string =
-                """
-                \n__
-                \(tag) Request: \(nils(request.httpMethod)) \(nils(request.url))
-                \(tag) Headers: \(nils(logHeaders(request.allHTTPHeaderFields)))
-                \(tag) Body: \(nils(body))
-                ‾‾
-                """
-            logger.log(string, level: .info, tag: loggerTag, function: "")
-        }
-
-        func log(
-            _ response: URLResponse?,
-            _ request: URLRequest,
-            _ content: String,
-            _ error: NSError?,
-            startDate: Date,
-            date: Date
-        ) {
-            let duration = date.timeIntervalSince(startDate)
-            let urlResponse = response as? HTTPURLResponse
-            let loggingLevel: LoggingLevel = (urlResponse?.statusCode ?? 1000) < 400 ? .info : .error
-            let tag = "→"
-            let string =
-                """
-                \n__
-                \(tag) Request: \(nils(request.httpMethod)) \(nils(request.url))
-                \(tag) Response: \(nils(urlResponse?.statusCode)), Duration: \(String(format: "%0.3f", duration)) s
-                \(tag) Headers: \(nils(logHeaders(urlResponse?.allHeaderFields)))
-                \(tag) \(content)
-                \(tag) Error: \(nils(error))
-                ‾‾
-                """
-            logger.log(string, level: loggingLevel, tag: loggerTag, function: "")
-        }
-
-        func log(_ data: Data?, _ response: URLResponse?) -> String {
-            let urlResponse = response as? HTTPURLResponse
-            let body = data.flatMap { data -> String? in
-                if let type = urlResponse?.allHeaderFields["Content-Type"] as? String, isText(type: type) && data.count <= maxBodySize {
-                    return String(data: data, encoding: .utf8) ?? String(data: data, encoding: .ascii)
-                } else {
-                    return "\(data.count) bytes"
-                }
-            }
-            return "Data: \(nils(body))"
-        }
-
-        func log(_ url: URL?) -> String {
-            "URL: \(nils(url?.absoluteString))"
-        }
-
-        private func nils(_ object: Any?) -> String {
-            object.map { "\($0)" } ?? "nil"
-        }
-
-        private func logHeaders(_ httpHeaders: [AnyHashable: Any]?) -> String? {
-            let headers = httpHeaders?.map { key, value -> String in "\(key): \(value)" }
-            return headers.map { "[\n    " + $0.joined(separator: "\n    ") + "\n  ]" }
-        }
-
-        private func isText(type: String) -> Bool {
-            type.contains("json") || type.contains("xml") || type.contains("text") || type.contains("www-form-urlencoded")
-        }
-    }
-
     // MARK: - Routines
 
     private enum Routines {
         static func processError(_ error: Error) -> HttpError {
-            let urlError = error as NSError
-            guard urlError.domain == NSURLErrorDomain else { return HttpError.error(error) }
+            let nsError = error as NSError
+            guard nsError.domain == NSURLErrorDomain else { return .error(error) }
 
-            switch urlError.code {
+            switch nsError.code {
                 case NSURLErrorTimedOut, NSURLErrorCannotFindHost, NSURLErrorCannotConnectToHost, NSURLErrorNetworkConnectionLost,
                      NSURLErrorDNSLookupFailed, NSURLErrorNotConnectedToInternet:
-                    return HttpError.unreachable(error)
+                    return .unreachable(error)
                 default:
-                    return HttpError.error(error)
+                    return .error(error)
             }
         }
     }
---
+++ Sources/Network/Http/UrlSessionHttpLogger.swift
@@ -0,0 +1,112 @@
+//
+// UrlSessionHttpLogger
+// Legacy
+//
+// Copyright (c) 2015 Eugene Egorov.
+// License: MIT, https://github.com/eugeneego/legacy/blob/master/LICENSE
+//
+
+import Foundation
+
+public protocol UrlSessionHttpLogger {
+    func log(_ request: URLRequest, date: Date)
+
+    func log(
+        _ response: URLResponse?,
+        _ request: URLRequest,
+        _ content: String,
+        _ error: NSError?,
+        startDate: Date,
+        date: Date
+    )
+
+    func log(_ data: Data?, _ response: URLResponse?) -> String
+
+    func log(_ url: URL?) -> String
+}
+
+open class DefaultUrlSessionHttpLogger: UrlSessionHttpLogger {
+    public let logger: Logger
+    public let loggerTag: String
+
+    open var maxBodySize: Int = 8192
+
+    public init(logger: Logger, loggerTag: String = String(describing: UrlSessionHttp.self)) {
+        self.logger = logger
+        self.loggerTag = loggerTag
+    }
+
+    open func log(_ request: URLRequest, date: Date) {
+        let tag = "←"
+        let body = request.httpBody.flatMap { data -> String? in
+            if let type = request.allHTTPHeaderFields?["Content-Type"], isText(type: type) && data.count <= maxBodySize {
+                return String(data: data, encoding: .utf8) ?? String(data: data, encoding: .ascii)
+            } else {
+                return "\(data.count) bytes"
+            }
+        } ?? request.httpBodyStream.map { _ in "stream" }
+        let string =
+            """
+            \n__
+            \(tag) Request: \(nils(request.httpMethod)) \(nils(request.url))
+            \(tag) Headers: \(nils(logHeaders(request.allHTTPHeaderFields)))
+            \(tag) Body: \(nils(body))
+            ‾‾
+            """
+        logger.log(string, level: .info, tag: loggerTag, function: "")
+    }
+
+    open func log(
+        _ response: URLResponse?,
+        _ request: URLRequest,
+        _ content: String,
+        _ error: NSError?,
+        startDate: Date,
+        date: Date
+    ) {
+        let duration = date.timeIntervalSince(startDate)
+        let urlResponse = response as? HTTPURLResponse
+        let loggingLevel: LoggingLevel = (urlResponse?.statusCode ?? 1000) < 400 ? .info : .error
+        let tag = "→"
+        let string =
+            """
+            \n__
+            \(tag) Request: \(nils(request.httpMethod)) \(nils(request.url))
+            \(tag) Response: \(nils(urlResponse?.statusCode)), Duration: \(String(format: "%0.3f", duration)) s
+            \(tag) Headers: \(nils(logHeaders(urlResponse?.allHeaderFields)))
+            \(tag) \(content)
+            \(tag) Error: \(nils(error))
+            ‾‾
+            """
+        logger.log(string, level: loggingLevel, tag: loggerTag, function: "")
+    }
+
+    open func log(_ data: Data?, _ response: URLResponse?) -> String {
+        let urlResponse = response as? HTTPURLResponse
+        let body = data.flatMap { data -> String? in
+            if let type = urlResponse?.allHeaderFields["Content-Type"] as? String, isText(type: type) && data.count <= maxBodySize {
+                return String(data: data, encoding: .utf8) ?? String(data: data, encoding: .ascii)
+            } else {
+                return "\(data.count) bytes"
+            }
+        }
+        return "Data: \(nils(body))"
+    }
+
+    open func log(_ url: URL?) -> String {
+        "URL: \(nils(url?.absoluteString))"
+    }
+
+    private func nils(_ object: Any?) -> String {
+        object.map { "\($0)" } ?? "nil"
+    }
+
+    private func logHeaders(_ httpHeaders: [AnyHashable: Any]?) -> String? {
+        let headers = httpHeaders?.map { key, value -> String in "\(key): \(value)" }
+        return headers.map { "[\n    " + $0.joined(separator: "\n    ") + "\n  ]" }
+    }
+
+    private func isText(type: String) -> Bool {
+        type.contains("json") || type.contains("xml") || type.contains("text") || type.contains("www-form-urlencoded")
+    }
+}
